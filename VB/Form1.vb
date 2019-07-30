5690c492a2ab93726f37009fb7aaf42f022dc51e
Kseniya Kuzmina <kseniya.kuzmina@devexpress.com>
2019-07-30 18:04:09 +0300

    Add vb version

---
+++ VB/Form1.vb
@@ -0,0 +1,105 @@
+﻿Imports DevExpress.Utils
+Imports DevExpress.XtraCharts
+Imports DevExpress.XtraEditors
+Imports System.Collections.Generic
+Imports System.Drawing
+
+Namespace DrillDownChart
+    Partial Public Class Form1
+        Inherits XtraForm
+
+        Private categories As List(Of String)
+        Private linkFont As Font
+        Private regularFont As Font
+        Private ReadOnly Property chartControl() As ChartControl
+            Get
+                Return chartControl1
+            End Get
+        End Property
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+            Dim keys = SaleItem.CategorizedProducts.Keys
+            categories = New List(Of String)(keys.Count)
+            For Each category As String In SaleItem.CategorizedProducts.Keys
+                categories.Add(category)
+            Next category
+
+            Dim data As List(Of SaleItem) = SaleItem.GetTotalIncome()
+            chartControl.DataSource = data
+            chartControl.SeriesTemplate.SeriesDataMember = "Category"
+            chartControl.SeriesTemplate.ArgumentDataMember = "Company"
+            chartControl.SeriesTemplate.QualitativeSummaryOptions.SummaryFunction = "SUM([Income])"
+            chartControl.SeriesTemplate.View = New StackedBarSeriesView()
+
+            Dim argumentDrillTemplate As New SeriesTemplate()
+            argumentDrillTemplate.SeriesDataMember = "Category"
+            argumentDrillTemplate.ArgumentDataMember = "OrderDate"
+            argumentDrillTemplate.View = New StackedAreaSeriesView()
+            argumentDrillTemplate.DateTimeSummaryOptions.MeasureUnit = DateTimeMeasureUnit.Month
+            argumentDrillTemplate.DateTimeSummaryOptions.UseAxisMeasureUnit = False
+            argumentDrillTemplate.DateTimeSummaryOptions.SummaryFunction = "SUM([Income])"
+            chartControl.SeriesTemplate.ArgumentDrillTemplate = argumentDrillTemplate
+
+            Dim seriesDrillTemplate As New SeriesTemplate()
+            seriesDrillTemplate.SeriesDataMember = "Product"
+            seriesDrillTemplate.ArgumentDataMember = "Company"
+            seriesDrillTemplate.QualitativeSummaryOptions.SummaryFunction = "SUM([Income])"
+            seriesDrillTemplate.View = New StackedBarSeriesView()
+            chartControl.SeriesTemplate.SeriesDrillTemplate = seriesDrillTemplate
+
+            Dim seriesPointDrillTemplate As New SeriesTemplate()
+            seriesPointDrillTemplate.SeriesDataMember = "Product"
+            seriesPointDrillTemplate.ArgumentDataMember = "OrderDate"
+            seriesPointDrillTemplate.DateTimeSummaryOptions.MeasureUnit = DateTimeMeasureUnit.Month
+            seriesPointDrillTemplate.DateTimeSummaryOptions.UseAxisMeasureUnit = False
+            seriesPointDrillTemplate.DateTimeSummaryOptions.SummaryFunction = "SUM([Income])"
+            seriesPointDrillTemplate.View = New StackedAreaSeriesView()
+            chartControl.SeriesTemplate.SeriesPointDrillTemplate = seriesPointDrillTemplate
+
+            Dim diagram As XYDiagram = CType(chartControl.Diagram, XYDiagram)
+            If (Not (diagram) Is Nothing) Then
+                diagram.Rotated = True
+                regularFont = diagram.AxisX.Label.Font
+                linkFont = New Font(regularFont, FontStyle.Underline)
+                diagram.AxisX.Label.Font = linkFont
+            End If
+
+            AddHandler chartControl.DrillDownStateChanged, AddressOf chart_DrillDownStateChanged
+        End Sub
+
+        Private Sub chart_DrillDownStateChanged(ByVal sender As Object, ByVal e As DrillDownStateChangedEventArgs)
+            Dim diagram As XYDiagram = TryCast(chartControl.Diagram, XYDiagram)
+
+            If (diagram IsNot Nothing) AndAlso (e.Series.Length > 0) Then
+
+                If TypeOf e.Series(0).View Is StackedBarSeriesView Then
+                    chartControl.CrosshairEnabled = DefaultBoolean.[False]
+                    chartControl.ToolTipEnabled = DefaultBoolean.[True]
+                    diagram.Rotated = True
+                    diagram.AxisX.Label.Font = Me.linkFont
+                    diagram.EnableAxisXScrolling = False
+                    diagram.EnableAxisXZooming = False
+                Else
+                    chartControl.CrosshairEnabled = DefaultBoolean.[True]
+                    chartControl.ToolTipEnabled = DefaultBoolean.[False]
+                    diagram.Rotated = False
+                    diagram.AxisX.Label.Font = Me.regularFont
+                    diagram.EnableAxisXScrolling = True
+                    diagram.EnableAxisXZooming = True
+                End If
+            End If
+
+            For Each item As DrillDownItem In e.States
+
+                If item.Parameters.ContainsKey("Category") Then
+                    chartControl.PaletteBaseColorNumber = categories.IndexOf(item.Parameters("Category").ToString()) + 1
+                    Return
+                End If
+            Next
+
+            chartControl.PaletteBaseColorNumber = 0
+        End Sub
+    End Class
+End Namespace
---
+++ VB/SaleItem.vb
@@ -0,0 +1,98 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Namespace DrillDownChart
+	Friend Class SaleItem
+		Private ReadOnly Shared companies() As String = { "DevAV North", "DevAV South", "DevAV West", "DevAV East", "DevAV Central" }
+'INSTANT VB NOTE: The field categorizedProducts was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private Shared categorizedProducts_Renamed As Dictionary(Of String, List(Of String))
+		Friend Shared ReadOnly Property CategorizedProducts() As Dictionary(Of String, List(Of String))
+			Get
+				If categorizedProducts_Renamed Is Nothing Then
+					categorizedProducts_Renamed = New Dictionary(Of String, List(Of String))()
+					categorizedProducts_Renamed("Cameras") = New List(Of String)() From {"Camera", "Camcorder", "Binoculars", "Flash", "Tripod"}
+					categorizedProducts_Renamed("Cell Phones") = New List(Of String)() From {"Smartphone", "Mobile Phone", "Smart Watch", "Sim Card"}
+					categorizedProducts_Renamed("Computers") = New List(Of String)() From {"Desktop", "Laptop", "Tablet", "Printer"}
+					categorizedProducts_Renamed("TV, Audio") = New List(Of String)() From {"Television", "Home Audio", "Headphone", "DVD Player"}
+					categorizedProducts_Renamed("Vehicle Electronics") = New List(Of String)() From {"GPS Unit", "Radar", "Car Alarm", "Car Accessories"}
+					categorizedProducts_Renamed("Multipurpose Batteries") = New List(Of String)() From {"Battery", "Charger", "Converter", "Tester", "AC/DC Adapter"}
+				End If
+				Return categorizedProducts_Renamed
+			End Get
+		End Property
+		Friend Shared Function GetTotalIncome() As List(Of SaleItem)
+			Dim rnd As New Random(Date.Now.Millisecond)
+			Dim now As Date = Date.Now
+			Dim endDate As New Date(now.Year, now.Month, 1)
+			Dim items As New List(Of SaleItem)()
+'INSTANT VB NOTE: The variable company was renamed since Visual Basic does not handle local variables named the same as class members well:
+			For Each company_Renamed As String In companies
+				Dim companyFactor As Double = rnd.NextDouble() * 0.6 + 1
+'INSTANT VB NOTE: The variable category was renamed since Visual Basic does not handle local variables named the same as class members well:
+				For Each category_Renamed As String In CategorizedProducts.Keys
+					Dim categoryFactor As Double = rnd.NextDouble() * 0.6 + 1
+'INSTANT VB NOTE: The variable product was renamed since Visual Basic does not handle local variables named the same as class members well:
+					For Each product_Renamed As String In CategorizedProducts(category_Renamed)
+						Dim maxIncome As Integer = rnd.Next(60, 140)
+						For i As Integer = 0 To 999
+							If i Mod 100 = 0 Then
+								maxIncome = Math.Max(40, rnd.Next(maxIncome - 20, maxIncome + 20))
+							End If
+							Dim [date] As Date = endDate.AddDays(-i - 1)
+'INSTANT VB NOTE: The variable income was renamed since Visual Basic does not handle local variables named the same as class members well:
+							Dim income_Renamed As Double = rnd.Next(20, maxIncome) * companyFactor * categoryFactor
+							items.Add(New SaleItem() With {
+								.Category = category_Renamed,
+								.Company = company_Renamed,
+								.Product = product_Renamed,
+								.OrderDate = [date],
+								.Income = income_Renamed
+							})
+						Next i
+					Next product_Renamed
+				Next category_Renamed
+			Next company_Renamed
+			Return items
+		End Function
+		Public Property Product() As String
+		Public Property Company() As String
+		Public Property OrderDate() As Date
+		Public Property Month() As String
+		Public Property Income() As Double
+		Public Property Revenue() As Double
+		Public Property Category() As String
+	End Class
+	Friend Class CategorySaleItem
+'INSTANT VB NOTE: The field saleItems was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private ReadOnly saleItems_Renamed As New List(Of SaleItem)()
+'INSTANT VB NOTE: The field productIncome was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private productIncome_Renamed As Dictionary(Of String, Double)
+		Public Property OrderIndex() As Integer
+		Public Property Category() As String
+		Public ReadOnly Property SaleItems() As List(Of SaleItem)
+			Get
+				Return saleItems_Renamed
+			End Get
+		End Property
+		Public ReadOnly Property TotalIncome() As Double
+			Get
+				Return saleItems_Renamed.Sum(Function(x) x.Income)
+			End Get
+		End Property
+		Public ReadOnly Property ProductIncome() As Dictionary(Of String, Double)
+			Get
+				If productIncome_Renamed Is Nothing Then
+					productIncome_Renamed = New Dictionary(Of String, Double)()
+					For Each item As SaleItem In SaleItems
+						If productIncome_Renamed.Keys.Contains(item.Product) Then
+							productIncome_Renamed(item.Product) += item.Income
+						Else
+							productIncome_Renamed(item.Product) = item.Income
+						End If
+					Next item
+				End If
+				Return productIncome_Renamed
+			End Get
+		End Property
+	End Class
+End Namespace
